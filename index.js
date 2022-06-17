957c81190f9ea4665f35d9b6e7aa6ccf23f46a0a
devdan <oluochdancun19@gmail.com>
2022-06-17 11:46:16 +0300

    js static

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase()
+  }
+
+  static sanitize( string ) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
 }
